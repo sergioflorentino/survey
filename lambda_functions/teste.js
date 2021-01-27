4edf9e60e43b360b38386989ba6454677e2da43b
Sergio Florentino <sergioflorentino@gmail.com>
2021-01-27 01:32:16 -0300

    db17

--- lambda_functions/teste.js
+++ lambda_functions/teste.js
@@ -5,7 +5,8 @@
 // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html
 module.exports.handler = async (event, context) => {
    
-   const MONGODB_URI = process.env.MONGODB_URI;
+   //const MONGODB_URI = process.env.MONGODB_URI;
+   const MONGODB_URI = '';
    return {
      statusCode: 200,
      body: `MONGODB_URI is ${MONGODB_URI}`,
