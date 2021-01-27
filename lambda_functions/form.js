d39593b6baa37a39457db79da1de674f96a31b29
Sergio Florentino <sergioflorentino@gmail.com>
2021-01-27 01:45:38 -0300

    db18

--- lambda_functions/form.js
+++ lambda_functions/form.js
@@ -1,77 +1,14 @@
 // ./lambda_functions/pokemon.js
 
-const connectToDatabase =  require('./helpers');
+const functions = require('./helpers');
 
-const queryDatabase = async (db , hash, limit) => {
-
-  // se não enviar hash  retorna todos os registros
-
-  const surveys = await db.collection("surveys").find( hash?{hash}:'').limit( parseInt(limit) ).toArray();
-
-  return {
-     statusCode: 200,
-     headers: {
-       "Content-Type": "application/json",
-     },
-     body: JSON.stringify(surveys),
-   };
-};
-
-  
-//module.exports.handler = async (event, context) => {
-    // otherwise the connection will never complete, since
-    // we keep the DB connection alive
-//    context.callbackWaitsForEmptyEventLoop = false;
-  
-//    const db = await connectToDatabase(MONGODB_URI);
-//    return queryDatabase(db);
-//};
-
-const pushToDatabase = async (db, data, collection) => {
-
-    if (collection == "surveys") {
-      const collect = {
-            content: data.questions,
-            hash: data.hash,
-          };
-    }
-    else if (collection == "responses") {
-      const collect = {
-            content: data.responses,
-            hash: data.hash,
-          };    
-        }
-
-  
-    if (collect.content && collect.hash) {
-      await db.collection(collection).insertMany([data]);
-      return { statusCode: 201 };
-    } else {
-      return { statusCode: 422 };
-    }
-};
-  
-const editDatabase = async (db, param) => {
-
-   if (param.updated) {
-     await db.collection("surveys").updateOne(
-       { hash: param.hash },
-       { $set: { "questions.$" : param.questions } }
-    )
-     return { statusCode: 201 };
-   } else {
-     return { statusCode: 422 };
-   }
-};
-
-
-const handler = async function (event, context) {
+module.exports.handler = async function (event, context) {
     // otherwise the connection will never complete, since
     // we keep the DB connection alive
     context.callbackWaitsForEmptyEventLoop = false;
     const MONGODB_URI = process.env.MONGODB_URI;
       
-    const db = await connectToDatabase(MONGODB_URI);
+    const db = await functions.connectToDatabase(MONGODB_URI);
     const hash = event.headers['hash'] ? event.headers.hash : '';
     const limit = event.headers['limit'] ? event.headers.limit : 100 ;
     const collection = event.headers['collection'] ? event.headers.collection : '';
@@ -79,14 +16,13 @@ const handler = async function (event, context) {
   
     switch (event.httpMethod) {
       case "GET":
-        return queryDatabase(db, hash, limit);
+        return functions.queryDatabase(db, hash, limit);
       case "POST":
-        return pushToDatabase(db, JSON.parse(event.body), collection);
+        return functions.pushToDatabase(db, JSON.parse(event.body), collection);
       case "PUT":
-        return editDatabase(db, inputdata);
+        return functions.editDatabase(db, inputdata);
       default:
         return { statusCode: 400 };
     }
 };
 
-exports.handler = handler;
--- lambda_functions/helpers.js
+++ lambda_functions/helpers.js
@@ -18,4 +18,60 @@ const connectToDatabase = async (uri) => {
   return cachedDb;
 };
 
-module.exports = connectToDatabase;
+const queryDatabase = async (db , hash, limit) => {
+
+   // se não enviar hash  retorna todos os registros
+ 
+   const surveys = await db.collection("surveys").find( hash?{hash}:'').limit( parseInt(limit) ).toArray();
+ 
+   return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(surveys),
+    };
+ };
+ 
+   
+ const pushToDatabase = async (db, data, collection) => {
+ 
+     if (collection == "surveys") {
+       const collect = {
+             content: data.questions,
+             hash: data.hash,
+           };
+     }
+     else if (collection == "responses") {
+       const collect = {
+             content: data.responses,
+             hash: data.hash,
+           };    
+         }
+ 
+   
+     if (collect.content && collect.hash) {
+       await db.collection(collection).insertMany([data]);
+       return { statusCode: 201 };
+     } else {
+       return { statusCode: 422 };
+     }
+ };
+   
+ const editDatabase = async (db, param) => {
+ 
+    if (param.updated) {
+      await db.collection("surveys").updateOne(
+        { hash: param.hash },
+        { $set: { "questions.$" : param.questions } }
+     )
+      return { statusCode: 201 };
+    } else {
+      return { statusCode: 422 };
+    }
+ };
+
+exports.connectToDatabase = connectToDatabase;
+exports.queryDatabase = queryDatabase;
+exports.pushToDatabase = pushToDatabase;
+exports.editDatabase = editDatabase;
